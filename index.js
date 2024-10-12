80f3c82b04a4263c8187481fc0509704caae519d
Atul Deep Singh <999.atulsingh@gmail.com>
2024-10-12 13:17:28 +0530

    introduced routers

--- index.js
+++ index.js
@@ -3,7 +3,11 @@ const {createUserRoutes} = require('./routes/user');
 const { createCourseRoutes } = require('./routes/course');
 const app= express();
 
-createUserRoutes(app);
+//ideal structure
+app.use('/api/v1/user', userRouter);  //export a router //using riuter fucntion
+app.use('/api/v1/course', courseRouter);
+
+createUserRoutes(app); //exporting a fiunction
 createCourseRoutes(app);
 
 app.listen(3000);
--- routes/user.js
+++ routes/user.js
@@ -1,26 +1,27 @@
 
+const {Router}=require('express');
+const userRouter=Router();
 
-function createUserRoutes(app){
-    app.post('/user/signup', function (req, res){
-        res.json({
-            message: "sign up endpoint"
-        })
+
+userRouter.post('/user/signup', function (req, res){
+    res.json({
+        message: "sign up endpoint"
     })
+})
 
-
-    app.post('/user/signin', function(req,res){
-        res.json({
-            message: "sign in endpoint"
-        })
+userRouter.post('/user/signin', function(req,res){
+    res.json({
+        message: "sign in endpoint"
     })
+})
 
-    app.get('/user/purchases', function(req,res){
-        res.json({
-            message: "what are the courses purched by user"
-        })
+userRouter.get('/user/purchases', function(req,res){
+    res.json({
+        message: "what are the courses purched by user"
     })
+})
 
-}
+
 
 
 moduile.exports = {
